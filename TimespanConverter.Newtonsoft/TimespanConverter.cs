c9773ac434dfe9aec93fad027991c56574f7c829
Giovanni Dipino <giovanni.dipino@ersistemi.it>
2021-06-25 10:49:27 +0200

    First commit

---
+++ TimespanConverter.Newtonsoft/TimespanConverter.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Jonnidip
+{
+    public class TimespanConverter : JsonConverter<TimeSpan>
+    {
+        public const string TimeSpanFormatString = @"d\.hh\:mm\:ss\:FFF";
+
+        public override void WriteJson(JsonWriter writer, TimeSpan value, JsonSerializer serializer)
+            => writer.WriteValue(value.ToString(TimeSpanFormatString));
+
+        public override TimeSpan ReadJson(JsonReader reader, Type objectType, TimeSpan existingValue, bool hasExistingValue, JsonSerializer serializer)
+            => reader.Value != null
+                ? TimeSpan.ParseExact((string)reader.Value, TimeSpanFormatString, null)
+                : new TimeSpan();
+    }
+}
